215a9b41dff2c22581ac70145be5d3acf2dffc51
Javad <deroom24@gmail.com>
2017-08-29 15:36:39 +0430

    kartabl

---
+++ WebPages/Dashboard/Admin/EmployeePassword.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataAccess;
+using DataAccess.Repository;
+
+namespace WebPages.Dashboard.Admin
+{
+    public partial class EmployeePassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSubmit_ServerClick(object sender, EventArgs e)
+        {
+            SchoolDBEntities db = new SchoolDBEntities();
+
+            Karmand stuu = db.Karmands.Where(p => p.UserName == "karim").Single();
+            if (tbxCurrentPassword.Value == stuu.UserPass)
+            {
+                stuu.UserPass = tbxNewPassword.Value;
+                db.SaveChanges();
+            }
+        }
+    }
+}
